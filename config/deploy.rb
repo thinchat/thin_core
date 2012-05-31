646c36d2b498342ddf67ce9ad9bce0f5d8a0798d
Jonan Scheffler <jonanscheffler@gmail.com>
2012-05-31 17:20:19 -0400

    Try to deploy shared nginx.conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ namespace :deploy do
   end
 
   task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{shared_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
