c749e96a6f5de76c7ba7755cc54e9539f3b6cb3c
Edward Weng <edweng@gmail.com>
2012-06-04 10:07:24 -0400

    Fixing merge conflicts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,6 @@ class MessagesController < ApplicationController
   respond_to :html, :json, :js
 
   def create
-    unless @message = Message.create!(params[:message])
     if @message = Message.create!(params[:message])
       broadcast("/messages/#{@message.room_id}", @message)
       render :json => @message
