945e2f4e1010d0d965a3f6465ea19f89609965fd
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-04 10:38:38 -0400

    User localhost for faye except in production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block)}
-    uri = URI.parse("http://thinchat.com:9292/faye")
+    uri = URI.parse("#{faye_url}/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
+  end
+
+  def faye_url
+    if Rails.env == 'production'
+      'http://thinchat.com:9292'
+    else
+      'http://localhost:9292'
+    end
   end
 end
