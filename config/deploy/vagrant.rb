a647a323991f01ecfa658d4feb4ecf337ed4a295
Chris Anderson <christopher.anderson@gmail.com>
2012-06-07 14:35:22 -0400

    added the ruby racer

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,10 +1,13 @@
-server "27.27.27.27", :web, :app, :db, primary: true
-set :port, 2222
+set :host, "localhost"
+role :web, host
+role :app, host
+role :db, host, :primary => true
+ssh_options[:port] = "2222"
 
 set :application, "thin_core"
-set :user, "vagrant"
+set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
-set :deploy_via, :remote_cache
+# set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
