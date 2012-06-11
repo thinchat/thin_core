4cf68e0921430ffaa284c0acc3b80500653eb672
Edward Weng <edweng@gmail.com>
2012-06-11 10:01:15 -0400

    Deleting some files

--- app/controllers/api/v1/rooms_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::RoomsController < ApplicationController
-  def show
-    if @room = Room.where(id: params[:id].to_s).first
-      render :json => @room.messages, :status => :found
-    else
-      render :json => {:error => "Room not found."}, :status => :not_found
-    end
-  end
-end
