c3fec36fcf90fe847398a6b50321a3a0d2a43433
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-11 14:23:18 -0400

    Use p194

--- config/vagrant/setup.sh
+++ config/vagrant/setup.sh
@@ -5,7 +5,7 @@ apt-get -y upgrade
 apt-get -y install git-core curl vim libffi-dev
 echo "America/New_York" | sudo tee /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
-curl -L https://raw.github.com/gist/2828151/ff2d251c9f4f149c5ca73c873ad8990711b3ca74/chef_solo_bootstrap.sh | bash
+curl -L https://raw.github.com/gist/2910634/8ddbbe9c3c95782bc37bc0a1d261265d5afedebe/chef_solo_bootstrap.sh | bash
 gem install chef ruby-shadow --no-ri --no-rdoc
 gem install bundler
 gem install god
