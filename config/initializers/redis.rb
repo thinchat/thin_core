738834a8c8998549b298eee955fe50f9207d9e0c
Edward Weng <edweng@gmail.com>
2012-06-11 10:25:59 -0400

    Removing secret

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 REDIS = Redis.new(:host => 'localhost', :port => 6379)
 if Rails.env == 'production'
-  require "#{Rails.root}/config/initializers/secret.rb"
+  require "#{Rails.root}/config/secret/redis_password.rb"
   REDIS.auth(REDIS_PASSWORD)
 end
