2038eb31427df3396cd7c1ed0e8ac3788893528c
Edward Weng <edweng@gmail.com>
2012-06-12 16:25:06 -0400

    Fixing localhost URLs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     password = Rails.env == 'production' ? REDIS_PASSWORD : nil
-    @users = ThinHeartbeat::Status.new(password).get_users
+    @users = ThinHeartbeat::Status.new(REDIS_URL, password).get_users
     render :json => @users
   end
 end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,13 @@
-REDIS = Redis.new(:host => 'localhost', :port => 6379)
+if Rails.env == 'production'
+  REDIS_URL = 'http://thinchat.com'
+elsif Rails.env == 'staging'
+  REDIS_URL = 'http://50.116.40.131'
+else
+  REDIS_URL = 'localhost'
+end
+
+REDIS = Redis.new(:host => REDIS_URL, :port => 6379)
+
 if Rails.env == 'production'
   require "#{Rails.root}/config/secret/redis_password.rb"
   REDIS.auth(REDIS_PASSWORD)
