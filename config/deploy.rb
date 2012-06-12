e81b3a4d2147244d0eeac044b6488795b0d72f1f
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-11 23:08:34 -0400

    Unicorn env

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+      run "/etc/init.d/unicorn_#{application} #{command} -E #{rails_env}"
     end
   end
 
