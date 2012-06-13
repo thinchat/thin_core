b211552657055cc14e0a70e7e3594790ec1cea61
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-13 10:08:18 -0400

    REDIS_URL error handling

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,16 +1,14 @@
-if Rails.env == 'production'
-  REDIS_URL = 'http://thinchat.com'
+if Rails.env == 'development'
+  REDIS_URL = 'localhost'
 elsif Rails.env == 'staging'
   REDIS_URL = 'http://50.116.40.131'
+elsif Rails.env == 'production'
+  REDIS_URL = 'http://thinchat.com'
 else
-  REDIS_URL = 'localhost'
+  raise NotImplementedError, "The environment '#{Rails.env}' has no REDIS_URL\n Set one in config/initializers/redis.rb"
 end
 
 REDIS = Redis.new(:host => REDIS_URL, :port => 6379)
 
-PASSWORD_REQUIRED = ['production', 'staging']
-
-if PASSWORD_REQUIRED.include? Rails.env
-  require "#{Rails.root}/config/secret/redis_password.rb"
-  REDIS.auth(REDIS_PASSWORD)
-end
+require "#{Rails.root}/config/secret/redis_password.rb"
+REDIS.auth(REDIS_PASSWORD)
