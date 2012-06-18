128f6e5270df4f38511cbd1427eda9b7c4b6ff69
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-18 19:50:09 -0400

    Kill the old pid if the new worker starts successfully

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,3 +14,16 @@ timeout 30
 after_fork do |server, worker|
   Resque.redis = Redis.new(:host => 'localhost', :port => 6379)
 end
+
+before_fork do |server, worker|
+  # a .oldbin file exists if unicorn was gracefully restarted with a USR2 signal
+  # we should terminate the old process now that we're up and running
+  old_pid = "#{pids_dir}/unicorn.pid.oldbin"
+  if File.exists?(old_pid)
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
