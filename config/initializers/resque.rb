fd6b190e4bfafa32af67f1febe7d818e3b0e1e09
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-18 08:50:44 -0400

    After_fork for resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,3 +2,8 @@ require 'resque'
 require 'resque/server'
 
 Resque.redis = 'localhost:6379'
+
+Resque.after_fork = Proc.new {
+  ActiveRecord::Base.retrieve_connection
+  Resque.redis = Redis.new $resque_redis_config
+}
