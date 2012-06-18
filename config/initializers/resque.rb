ed33328979d6255ed58e0f9c0bfcc38cea046080
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-18 09:07:16 -0400

    Reconnect redis after unicorn fork

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,8 +2,3 @@ require 'resque'
 require 'resque/server'
 
 Resque.redis = 'localhost:6379'
-
-Resque.after_fork = Proc.new {
-  ActiveRecord::Base.retrieve_connection
-  Resque.redis = Redis.new $resque_redis_config
-}
